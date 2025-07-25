148e4c1fd3fdf1edb7558f78923312013afc0a91
njlyon0 <nicholasjlyon@gmail.com>
2025-07-25 12:32:25 -0400

    feat (setup): adding download of 'unjoined raw data'

--- 00_setup.R
+++ 00_setup.R
@@ -19,15 +19,33 @@ librarian::shelf(tidyverse, googledrive)
 rm(list = ls()); gc()
 
 ## -------------------------------------------- ##
-# Folders -----
+# Folders ----
 ## -------------------------------------------- ##
 
 # Create needed folders
 dir.create(path = file.path("data", "raw"), showWarnings = F, recursive = T)
+dir.create(path = file.path("data", "raw", "unjoined"), showWarnings = F)
 dir.create(path = file.path("data", "tidy"), showWarnings = F)
 
 ## -------------------------------------------- ##
-# Raw Data -----
+# 'Unjoined' Data ----
+## -------------------------------------------- ##
+
+# These are data files where one LTER had more than one raw file so pre-harmonization joining needed to happen
+
+# Define relevant Drive folder
+drive_unjoin <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1c0_FMLDpi7AVURRFy-4q4m1j0mBK0Xf8")
+
+# Identify data files in that folder
+(unjoin_files <- googledrive::drive_ls(path = drive_unjoin, pattern = "*.csv"))
+
+# Download all of these (overwriting local copies if any exist)
+purrr::walk2(.x = unjoin_files$id, .y = unjoin_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", "raw", "unjoined", .y)))
+
+## -------------------------------------------- ##
+# Raw Data ----
 ## -------------------------------------------- ##
 
 # Define relevant Drive folder
