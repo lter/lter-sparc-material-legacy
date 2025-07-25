603c865d6a2fceb3446d9bab0dd8836be37145cf
njlyon0 <nicholasjlyon@gmail.com>
2025-07-25 12:39:48 -0400

    feat (pre-process): adding script skeletons for sites where joining is necessary

---
+++ pre-process/pre-process_hfr.R
@@ -0,0 +1,27 @@
+## --------------------------------------------------------------------- ##
+# Material Legacy - Pre-Processing
+## --------------------------------------------------------------------- ##
+# Code author(s): Nick J Lyon, 
+
+# Purpose
+## Harmonization expects one data file per site
+## This code does needed joining to get from multiple raw files to a single raw (mostly) file
+## This script works on data from: Harvard Forest (HFR) LTER
+
+## -------------------------------------------- ##
+# Housekeeping ----
+## -------------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls()); gc()
+
+## -------------------------------------------- ##
+#  ----
+## -------------------------------------------- ##
+
+
+
+# End ----
