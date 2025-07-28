31b543a941d51d6d5a52691156d6e121a2fc3f46
njlyon0 <nicholasjlyon@gmail.com>
2025-07-28 10:14:00 -0400

    feat (harmonize): script skeleton

---
+++ 01_harmonize.R
@@ -0,0 +1,35 @@
+## --------------------------------------------------------------------- ##
+# Material Legacy - Harmonization
+## --------------------------------------------------------------------- ##
+# Code author(s): Nick J Lyon, 
+
+# Purpose
+## Use a 'data key' to standardize column names and combine separate raw inputs (i.e., harmonize)
+
+# Pre-Requisites
+## Download raw data (can be done from `00_setup.R`)
+## Download data key (can be done from `00_setup.R`)
+
+## -------------------------------------------- ##
+# Housekeeping ----
+## -------------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, ltertools)
+
+# Clear environment
+rm(list = ls()); gc()
+
+## -------------------------------------------- ##
+#  ----
+## -------------------------------------------- ##
+
+
+
+## -------------------------------------------- ##
+#  ----
+## -------------------------------------------- ##
+
+
+
+# End ----
