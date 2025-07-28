0048d88488197ff4e56622b5eb9555ce39a2ac4a
njlyon0 <nicholasjlyon@gmail.com>
2025-07-28 10:58:13 -0400

    feat (harmonize): first pass at standard harmonization workflow

--- 01_harmonize.R
+++ 01_harmonize.R
@@ -15,21 +15,43 @@
 ## -------------------------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse, ltertools)
+librarian::shelf(tidyverse, ltertools, supportR)
 
 # Clear environment
 rm(list = ls()); gc()
 
 ## -------------------------------------------- ##
-#  ----
+# Prepare to Harmonize ----
 ## -------------------------------------------- ##
 
+# Read in key object
+key <- read.csv(file = file.path("data", "material-legacy_data-key.csv"))
 
+# Check structure
+dplyr::glimpse(key)
+
+# Any raw files not in the data key?
+supportR::diff_check(old = unique(key$source),
+                     new = dir(path = file.path("data", "raw"), 
+                               pattern = "*.csv"))
 
 ## -------------------------------------------- ##
-#  ----
+# Harmonize ----
 ## -------------------------------------------- ##
 
+# Harmonize the data
+tidy_v1 <- ltertools::harmonize(key = key, raw_folder = file.path("data", "raw"),
+                                data_format = "csv", quiet = F)
 
+# Check structure
+dplyr::glimpse(tidy_v1)
+
+## -------------------------------------------- ##
+# Export ----
+## -------------------------------------------- ##
+
+# Export this locally
+write.csv(tidy_v1, row.names = F, na = '',
+          file = file.path("data", "tidy", "01_harmonized.csv"))
 
 # End ----
