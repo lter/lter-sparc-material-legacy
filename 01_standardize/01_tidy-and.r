e63ba5346354ef9a76c782c3d81c92cf2e6f8c19
njlyon0 <nicholasjlyon@gmail.com>
2026-09-18 09:53:50 -0400

    feat (std): integrate AND wood tidying direct from EDI and delete superseded temp scripts of same purpose

--- 01_standardize/01_tidy-and.r
+++ 01_standardize/01_tidy-and.r
@@ -5,8 +5,8 @@
 ## Get these data into a standard format with that of other sites
 
 ## !!! DATA SOURCE NOTE !!!
-# Data taken from Google Drive (_not_ EDI)
-## Data downloaded from Google Drive by `_TEMPORARY_drive-download.r`
+# One (of 5) input files from Google Drive (_not_ EDI)
+## That file is downloaded from Google Drive by `_TEMPORARY_drive-download.r`
 ## !!! SEE ABOVE !!!
 
 # Load libraries
@@ -23,10 +23,60 @@ rm(list = ls()); gc()
 # Load Dead Wood Data ----
 ## -------------------------------------------- ##
 
-# Load dead data
-and_dead <- read.csv(file = file.path("data", "from-drive", "OHJA_downed wood summary_v2.csv")) %>% 
-  janitor::clean_names() %>% 
-  dplyr::mutate(stand_plot = paste(stand, plot))
+# Load in relevant data
+and_wood_v01 <- read.csv(file.path("data", "raw", "00_AND__TD01204.csv")) %>% 
+  janitor::clean_names()
+
+# Check structure
+dplyr::glimpse(and_wood_v01)
+
+## -------------------------------------------- ##
+# Filter & Tidy Wood Data ----
+## -------------------------------------------- ##
+
+# Filter to only desired rows
+and_wood_v02 <- and_wood_v01 %>% 
+  dplyr::filter(studyid == "OHJA") %>% 
+  # NL: '89 was absent from old ver of data so removing it here too
+  dplyr::filter(year != 1989)
+
+# Check structure
+dplyr::glimpse(and_wood_v02)
+
+# Tidy up plot & stand
+and_wood_v03 <- and_wood_v02 %>% 
+  dplyr::mutate(plot_og = gsub(pattern = " ", replacement = "", x = plot)) %>% 
+  dplyr::mutate(plot_num = as.numeric(gsub("R", "", plot_og))) %>% 
+  # DB: "[Need] to coallpase a few stands into a single stand/plot due to some oddities in our database structure"
+  dplyr::mutate(plot = dplyr::case_when(
+    # NL: Need to include "RS17" even though it wasn't in DB's list;
+    ## Checked against old ver of data and that stand only has one plot,
+    ## which means it has to be collapsed 
+    stand %in% c(paste0("RS0", c(4:5, 7:8)), paste0("RS", c(10, 12, 14:18))) ~ 99,
+    TRUE ~ plot_num))
+
+# Check gained & lost stand/plot combinations
+supportR::diff_check(old = unique(paste(and_wood_v03$stand, and_wood_v03$plot_num)),
+  new = unique(paste(and_wood_v03$stand, and_wood_v03$plot)))
+
+# Check structure
+dplyr::glimpse(and_wood_v03)
+
+## -------------------------------------------- ##
+# Summarize Wood Data ----
+## -------------------------------------------- ##
+
+# Summarize dead wood metrics by plot
+and_dead <- and_wood_v03 %>% 
+  dplyr::mutate(plot = ifelse(plot == 99, yes = 1, no = plot)) %>% 
+  dplyr::mutate(stand_plot = paste(stand, plot)) %>% 
+  dplyr::group_by(stand_plot, stand, plot, year) %>% 
+  dplyr::summarize(
+    total_volume = sum(wvol, na.rm = TRUE),
+    total_mass = sum(wmass, na.rm = TRUE),
+    total_cover = sum(pcover, na.rm = TRUE),
+    total_area = sum(surfarea, na.rm = TRUE),
+    .groups = "drop")
 
 # Check structure
 dplyr::glimpse(and_dead)
